b6f71414a9ac89bff22eddabd43b0ea7517689ed
Tae Young Lee <taeleeswe@gmail.com>
2024-04-05 17:23:39 -0400

    second commit

--- main.py
+++ main.py
@@ -9,9 +9,16 @@ menu = Menu()
 machine_on = True
 
 while machine_on:
-    choice = input("What Would you like? (espresso/latte/cappuccino): ").lower()
+    options = menu.get_items()
+    choice = input(f"What Would you like? ({options}): ").lower()
     if choice == "off":
         machine_on = False
     elif choice == "report":
         coffee_maker.report()
         money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            coffee_maker.make_coffee(drink)
